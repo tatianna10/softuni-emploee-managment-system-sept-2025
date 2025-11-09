06688859b67686e81d3fc15a0dcccc4f53c118df
Tatyana Atanasova <106607660+tatianna10@users.noreply.github.com>
2025-11-09 23:01:48 +0200

    Add base components

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+
+        <header className="header">
+            <div className="logo">
+                <span className="course">React Exercise - Components Deep Dive</span>
+            </div>
+        </header>
+    );
+}
--- resources.jsx
+++ resources.jsx
@@ -1,28 +1,5 @@
-
-<body>
-  {/* <!-- Header component --> */}
-  <header className="header">
-    <div className="logo">
-      <span className="course">React Exercise - Components Deep Dive</span>
-    </div>
-  </header>
-
-  {/* <!-- Main component  --> */}
-  <main className="main">
-    <section className="card users-container">
-      <div className="search-form">
-        <h2>
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user"
-            className="svg-inline--fa fa-user SearchBar_icon__cXpTg" role="img" xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512">
-            <path fill="currentColor"
-              d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z">
-            </path>
-          </svg>
-          <span>Users</span>
-        </h2>
-      </div>
-
+    // <section className="card users-container">
+     
       {/* <!-- Table component --> */}
       <div className="table-wrapper">
         {/* <!-- Overlap components  --> */}
@@ -248,7 +225,11 @@
           </button>
         </div>
       </div>
-    </section>
+    {/* </section> */}
+
+
+
+// --after main
 
     {/* <!-- User details component  --> */}
     <div className="overlay"> 
@@ -425,9 +406,3 @@
       </div>
     </div > 
 
-  </main >
-  {/* <!-- Footer component  --> */ }
-  < footer className = "footer" >
-    <p>This site is designed to be used for training purposes at SoftUni.</p>
-  </footer >
-</body>
